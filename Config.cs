a8705aa4f926cf65d547a100e1ebce8fbbd361de
Flash Kirby <flashkirby@gmail.com>
2017-05-10 23:17:51 +0100

    Add FX config options

--- Config.cs
+++ Config.cs
@@ -6,17 +6,26 @@
         public static bool ShowBossHealthBars = true;
         /// <summary>Show small health bars</summary>
         public static bool SmallHealthBars = false;
+
         /// <summary>Offset from edge of screen</summary>
         public static int HealthBarUIScreenOffset = 16;
         /// <summary>Y Distance between bars</summary>
         public static int HealthBarUIStackOffset = 6;
         /// <summary>Max number of bars to show</summary>
         public static int HealthBarUIMaxStack = 3;
+        /// <summary>Offset from edge of screen</summary>
+        public static float HealthBarUIScreenLength = 0.5f;
+
         /// <summary>How long to fade in/out healthbars</summary>
         public static int HealthBarUIFadeTime = 60;
         /// <summary>Factor of fade time to drop to when mouse is covering</summary>
         public static float HealthBarUIFadeHover = 0.75f;
-        /// <summary>Offset from edge of screen</summary>
-        public static float HealthBarUIScreenLength = 0.5f;
+
+        /// <summary>Should the healthbar dramatically fill up on entry</summary>
+        public static bool HealthBarFXFillUp = false;
+        /// <summary>Should the healthbar shake when depleted</summary>
+        public static bool HealthBarFXShake = false;
+        /// <summary>Should the healthbar show damage being chipped away</summary>
+        public static bool HealthBarFXChip = false;
     }
 }
