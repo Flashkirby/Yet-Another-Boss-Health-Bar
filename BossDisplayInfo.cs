e78f43d78cdd8ee2f070994381899d46fc679581
Flash Kirby <flashkirby@gmail.com>
2017-05-10 02:21:01 +0100

    Add more convenient check method for NPC tracking

--- BossDisplayInfo.cs
+++ BossDisplayInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Reflection;
 using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
 
 using Terraria;
 using Terraria.ModLoader;
@@ -42,7 +43,7 @@ namespace FKBossHealthBar
             }
         }
 
-        public static HealthBar GetHealthBarForNPC(int npcType)
+        public static HealthBar GetHealthBarForNPCOrNull(int npcType)
         {
             HealthBar hb = null;
             if (NPCHealthBars.TryGetValue(npcType, out hb))
@@ -54,6 +55,47 @@ namespace FKBossHealthBar
                 // A default healthbar
                 return new HealthBar(npcType);
             }
+        }
+
+        public static bool CanTrackNPCHealth(NPC npc)
+        {
+            // Cannot track non-active entities
+            if (!npc.active) return false;
+
+            // Don't track NPCs which are part of a shared life chain (except the head of course)
+            if (npc.realLife >= 0) return false;
+
+            // Cannot track npcs without a health bar
+            if(npc.immortal || npc.dontTakeDamage)
+            {
+                return false;
+            }
+
+            // The modded NPC doesn't want to show health?
+            if (npc.modNPC != null)
+            {
+                float scale = 1f;
+                Vector2 position = new Vector2(npc.position.X + npc.width / 2, npc.position.Y + npc.gfxOffY);
+                bool? result = npc.modNPC.DrawHealthBar(Main.HealthBarDrawSettings, ref scale, ref position);
+                if (result == false)
+                {
+                    return false;
+                }
+            }
+
+            // Someone has specified a healthbar for this NPC?
+            if (GetHealthBarForNPCOrNull(npc.type) != null)
+            {
+                return true;
+            }
+
+            // Otherwise if it's a boss then also sure 
+            if (npc.boss)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
